04880fc02d3dcab557b50a308d60dc544d6353aa
174cm <omnia7@naver.com>
2020-05-11 13:58:18 +0900

    Crawling/Overseas_Covid-19 2020_05_01 ~ 11 result update

--- Crawling/Overseas_Covid-19_Crawling/source/Overseas_covid-19_0.9_demo.py
+++ Crawling/Overseas_Covid-19_Crawling/source/Overseas_covid-19_0.9_demo.py
@@ -2,6 +2,8 @@
 @author: 174cm
 @version: 0.9
 @since: 2020-04-08
+@modify date: 2020-05-06
+@difference: 1.하루에 두개씩 올리는 건수가 발생하여 어느날인지 제목만으로는 판별이 불가능하여 업로드 텍스트를 크롤링하는 부분을 수정함.
 '''
 import urllib.request
 import pandas as pd
@@ -14,9 +16,13 @@ table = soup.find('table', {
                   'style': 'table-layout: fixed; border-width: 0.28pt; border-style: solid; border-color: rgb(0, 0, 0);'})  # 테이블의 위치
 trs = table.find_all('tr')
 temp = ['아시아', '중동', '아메리카', '유럽', '오세아니아', '아프리카', '기타']
-
+# 일자 출력 테스트
+date = soup.select('p:nth-child(7) > span > b')
+print(date[0].text.strip())
+'''
 for idx, tr in enumerate(trs):
     tds = tr.find_all('td')
+    dat = tr.find_all('&nbsp')
     if tds[0].text in temp:
         cont = tds[1].text  # 국가
         conv = tds[2].text  # 확진자
@@ -25,5 +31,4 @@ for idx, tr in enumerate(trs):
         cont = tds[0].text  # 국가
         conv = tds[1].text  # 확진자
         dead = tds[2].text  # 사망자
-
-    print(cont, conv,  dead)
+'''
--- Crawling/Overseas_Covid-19_Crawling/source/Overseas_covid-19_3.0.py
+++ Crawling/Overseas_Covid-19_Crawling/source/Overseas_covid-19_3.0.py
@@ -12,6 +12,7 @@ from sys import stderr
 import sys
 import pandas as pd
 from bs4 import BeautifulSoup
+import csv
 
 baseUrl = 'http://www.xn--now-po7lf48dlsm0ya109f.kr/infect/occurrence_info.do;jsessionid=ECE7F42FC9E3E16E014F66D4307272C7?infect_no=in_'
 addUrl = input('날짜를 입력하세요: ')
@@ -23,9 +24,10 @@ html = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser')
 table = soup.find('table')  # 테이블을 찾음
 trs = table.find_all('tr')
+date = soup.select('p:nth-child(7) > span > b')
 continent = ['아시아', '중동', '아메리카', '유럽', '오세아니아', '아프리카', '기타'] # 대륙이 [0]번째 list를 잡아먹음으로, 제외할 항목을 작성.
 toCsv = []
-
+ 
 for idx, tr in enumerate(trs):
     tds = tr.find_all('td')
     tmp = []
@@ -41,3 +43,9 @@ for idx, tr in enumerate(trs):
 
 df = pd.DataFrame(data=toCsv)
 df.to_csv('Overseas_covid-19_'+addUrl+'_result.csv', encoding='utf-8')
+'''
+f = open('Overseas_covid-19_'+addUrl+'_result.csv','a', newline='')
+wr = csv.writer(f)
+wr.writerow([date[0].text.strip()], encoding='utf-8')
+f.close()
+'''
